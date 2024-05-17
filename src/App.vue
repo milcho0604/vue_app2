2cf3bd4b1966a9339ac9004ab1fc78046686dcf0
milcho0604 <milcho0604@gmail.com>
2024-05-17 16:23:50 +0900

    vue기초

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MyCheckbox3 />
+    <MyButton1 />
+    <MySelect3 />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyCheckbox3 from './MyCheckbox3.vue'
+import MyButton1 from './MyButton1.vue'
+import MySelect3 from './MySelect3.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyCheckbox3, MyButton1,
+    MySelect3
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+div { vertical-align: top; margin: 10px; }
+img { margin: 10px; }
 </style>
